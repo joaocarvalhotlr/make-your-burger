ae18563e32471d14784e9bd6433587696960f9fe
joaocarvalhotlr <joaomarceloc03@gmail.com>
2021-12-19 22:20:58 -0300

    first commit

---
+++ src/components/Banner.vue
@@ -0,0 +1,31 @@
+<template>
+    <div id="main-banner">
+        <h1>Vue Burger</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'Banner'
+    }
+</script>
+
+<style scoped>
+    #main-banner {
+        background-image: url('/img/burger.jpg');
+        background-position: 0 -250px;
+        background-size: cover;
+        height: 500px;
+        display: flex;
+        align-items: center;
+        justify-content: flex-start;
+    }
+
+    #main-banner h1 {
+        color: #fff;
+        text-align: center;
+        font-size: 60px;
+        background-color: rgba(34, 34, 34, 0.555);
+        padding: 20px 40px;
+    }
+</style>
